43f33f300868e484f1461ab731b2915d3ec03c1a
huyhoangse97 <80676594+huyhoangse97@users.noreply.github.com>
2022-01-19 12:44:15 +0700

    Completed update project - exchange folder structure

---
+++ Assets/Objects/Effects/Destroy/BloodEffect/BloodPSController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BloodPSController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(DestroyTiming());
+    }
+
+    IEnumerator DestroyTiming(){
+        yield return new WaitForSeconds(2f);
+        Destroy(this.gameObject);
+    }
+}
