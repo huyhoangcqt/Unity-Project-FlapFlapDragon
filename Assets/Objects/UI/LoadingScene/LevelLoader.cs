f42fdf3e55e96bdec5ce9ac9bc6a0929683a374a
huyhoangse97 <80676594+huyhoangse97@users.noreply.github.com>
2022-02-09 16:03:40 +0700

    Add Decorate to Loading Scene

---
+++ Assets/Objects/UI/LoadingScene/LevelLoader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public void LoadScene(string _sceneName){
+        StartCoroutine(LoadAsynchronously(_sceneName));
+    }
+
+    IEnumerator LoadAsynchronously(string _sceneName){
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName: _sceneName);
+        while (!operation.isDone){
+            Debug.Log(operation.progress);
+            yield return null;
+        }
+    }
+}
